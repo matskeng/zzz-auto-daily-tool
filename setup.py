963656b0f3d3549d39d9817e2cae4e60e307819e
matskeng <matsumoto.kengo.0205@gamil.com>
2024-11-17 16:30:53 +0900

    setup.py削除

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "zzz_auto_daily_tool",
-    version = "0.0.1",
-    description = "ZZZ Auto Daily Tool",
-    executables = [Executable("ZzzAutoDailyTool/__main_.py")]
-)
