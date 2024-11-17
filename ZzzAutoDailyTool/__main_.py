805bc5fe403f8fd0604c4fb7a57206f0d3cf6a51
matskeng <matsumoto.kengo.0205@gamil.com>
2024-11-17 23:09:47 +0900

    __main__.pyのファイル名を修正

--- ZzzAutoDailyTool/__main_.py
+++
@@ -1,3 +0,0 @@
-if __name__ == "__main__":
-    from .zzz_auto_daily_tool import main
-    main()
---
+++ ZzzAutoDailyTool/__main__.py
@@ -0,0 +1,5 @@
+from .lib.screen_rcg_and_click import screen_rcg_and_click
+from .zzz_auto_daily_tool import main
+
+if __name__ == "__main__":
+    main()
